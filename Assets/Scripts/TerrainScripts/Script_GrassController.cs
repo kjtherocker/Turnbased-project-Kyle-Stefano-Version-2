a49a9fd4753334647caa3edb9ad9d4b66f7821e4
kjtherocker <kylejohnsonthegod@gmail.com>
2018-08-22 16:47:04 -0400

    Added the Eye if red eyes

--- Assets/Scripts/TerrainScripts/Script_GrassController.cs
+++ Assets/Scripts/TerrainScripts/Script_GrassController.cs
@@ -9,10 +9,11 @@ public class Script_GrassController : MonoBehaviour
 
     public float radius = 0.5f;
     public float softness = 0.5f;
+    public Vector3 CirclePosition;
 
     void Update()
     {
-        Shader.SetGlobalVector("_GLOBALMaskPosition", transform.position);
+        Shader.SetGlobalVector("_GLOBALMaskPosition", CirclePosition);
         Shader.SetGlobalFloat("_GLOBALMaskRadius", radius);
         Shader.SetGlobalFloat("_GLOBALMaskSoftness", softness);
     }
